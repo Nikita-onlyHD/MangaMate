8a6f6ede94ece019ee301c4a332db19133089415
Nikita-onlyHD <darkdantari@gmail.com>
2025-06-07 20:55:12 +0300

    UserContext add event

--- UserContext.cs
+++ UserContext.cs
@@ -1,10 +1,69 @@
-﻿namespace MangaMate
+﻿using System.ComponentModel;
+
+namespace MangaMate
 {
     public static class UserContext
     {
-        public static int Id { get; set; } 
-        public static string Login { get; set; } = String.Empty;
-        public static string Email { get; set; } = String.Empty;
-        public static byte[]? Avatar { get; set; }
+        private static int _id;
+        public static int Id { 
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        private static string _login = string.Empty;
+        public static string Login
+        {
+            get => _login;
+            set
+            {
+                if (_login != value)
+                {
+                    _login = value;
+                    OnPropertyChanged(nameof(Login));
+                }
+            }
+        }
+
+        private static string _email = string.Empty;
+        public static string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        private static byte[]? _avatar;
+        public static byte[]? Avatar
+        {
+            get => _avatar;
+            set
+            {
+                if (_avatar != value)
+                {
+                    _avatar = value;
+                    OnPropertyChanged(nameof(Avatar));
+                }
+            }
+        }
+
+        public static event PropertyChangedEventHandler? PropertyChanged;
+
+        private static void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
